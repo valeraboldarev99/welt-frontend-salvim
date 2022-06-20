cc21bf10ce9f862b32b8651707c8c6cd2b447a4f
Valera Boldarev <vboldarev@weltkind.com>
2022-06-20 11:41:33 +0600

    main page PC

--- js/core.js
+++ js/core.js
@@ -1,3 +1,27 @@
 $(document).ready(function () {
+	// слaйдер на главной
+	$('.slider').slick({
+		slidesToScroll:1,
+		slidesToShow:1,
+		// autoplay: true,
+		// appendArrows:'.slider-arrows',
+		autoplaySpeed: 3000,
+		pauseOnHover:true,
+		arrows:true,
+		dots:false,
+		responsive: [
+			{
+				breakpoint: 767,
+				settings: {
+					arrows:true,
+				}
+			}
+		]
+	});
 
+	/*кнопка вверх*/
+	$('.scrollup').click(function(){
+		$("html, body").animate({ scrollTop: 0 }, 600);
+		return false;
+	});
 });
